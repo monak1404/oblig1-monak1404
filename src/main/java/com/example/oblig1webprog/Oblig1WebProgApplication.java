1cfb3ba14dbf67161dd94d42e042b51a09d3807a
monak1404 <monak2209@gmail.com>
2023-03-17 12:39:48 +0100

    ferdig

---
+++ src/main/java/com/example/oblig1webprog/Oblig1WebProgApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig1webprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig1WebProgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig1WebProgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oblig1webprog/Oblig1WebProgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1webprog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1WebProgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
